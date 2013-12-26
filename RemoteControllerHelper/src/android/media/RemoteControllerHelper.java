46ae30269cf4f760ddab348b229c68008698ccff
WisdomWolf <wisdomwolf@gmail.com>
2013-12-26 16:56:15 -0500

    initial commit

---
+++ RemoteControllerHelper/src/android/media/RemoteControllerHelper.java
@@ -0,0 +1,84 @@
+package android.media;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+/*
+ * Provides access to fields and methods from outside the android.media package
+ * Including:
+ * 	RemoteController
+ * 		-getArtworkSize()
+ * 		-getRcDisplay()
+ * 		-setIsRegistered(bool)
+ * 		-getRemoteControlClientPackageName()
+ * 
+ * Also provides access alternative method for RemoteController registration.
+ */
+
+public class RemoteControllerHelper{
+	private RemoteController mRemoteController;
+	private Context mContext;
+	private static IAudioService sService;
+	
+	
+	public RemoteControllerHelper(RemoteController remoteController, Context context){
+		mRemoteController = remoteController;
+		mContext = context;
+	}
+	
+	private static IAudioService getService() {
+        if (sService != null) {
+            return sService;
+        }
+        IBinder b = ServiceManager.getService(Context.AUDIO_SERVICE);
+        sService = IAudioService.Stub.asInterface(b);
+        return sService;
+    }
+	
+	public String getRemoteControlClientPackageName() {
+		return mRemoteController.getRemoteControlClientPackageName();
+	}
+	
+	public boolean setIsRegistered(boolean registered) {
+		return mRemoteController.setIsRegistered(registered);
+	}
+	
+	public RcDisplay getRcDisplay() {
+		return mRemoteController.getRcDisplay();
+	}
+	
+	public int[] getArtworkSize() {
+		return mRemoteController.getArtworkSize();
+	}
+	
+	public boolean registerRemoteController(RemoteController rctlr) {
+        if (rctlr == null) {
+            return false;
+        }
+        IAudioService service = getService();
+        final RemoteController.OnClientUpdateListener l = rctlr.getUpdateListener();
+        final ComponentName listenerComponent;
+        
+        //use actual class name if EnclosingClass is null
+        if (l.getClass().getEnclosingClass() == null){
+        	listenerComponent = new ComponentName(mContext, l.getClass());
+        } else {
+        	listenerComponent = new ComponentName(mContext, l.getClass().getEnclosingClass());
+        }
+        
+        try {
+            int[] artworkDimensions = rctlr.getArtworkSize();
+            boolean reg = service.registerRemoteController(rctlr.getRcDisplay(),
+                    artworkDimensions[0]/*w*/, artworkDimensions[1]/*h*/,
+                    listenerComponent);
+            rctlr.setIsRegistered(reg);
+            return reg;
+        } catch (RemoteException e) {
+            Log.e(TAG, "Dead object in registerRemoteController " + e);
+            return false;
+        }
+    }
+}
